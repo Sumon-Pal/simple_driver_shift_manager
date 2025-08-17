c66ae1f032c2afbfeb822660acf9237efbeaa4bb
Sumon-Pal <sumon.pal.ice@gmail.com>
2025-08-17 15:26:21 +0600

    Designed Sign In Screen

--- lib/driver_app.dart
+++ lib/driver_app.dart
@@ -7,7 +7,31 @@ class DriverApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SignInScreen(),
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: Colors.white,
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          hintStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(170, 50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 12),
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -8,22 +8,100 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _rememberMe = false;
+  bool _obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                SizedBox(height: 155),
             Text(
               "Sign In",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700,),
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+                fontFamily: "Mulish",
+              ),
             ),
-            Text("Please sign in to continue",style: TextStyle(fontSize:14, fontWeight: FontWeight.w400),),
-
-          ],
+            SizedBox(height: 9),
+            Text(
+              "Please sign in to continue",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                fontFamily: "Mulish",
+              ),
+            ),
+            SizedBox(height: 32),
+            TextFormField(
+              controller: _emailTEController,
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(labelText: "Email"),
+            ),
+            SizedBox(height: 26),
+            TextFormField(
+              controller: _passwordTEController,
+              textInputAction: TextInputAction.done,
+              decoration: InputDecoration(
+                labelText: "Password",
+                suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscurePassword ? Icons.visibility_off : Icons
+                          .visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    }),
+              ),
+            ),
+              SizedBox(height: 28),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _rememberMe,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _rememberMe = value ?? false;
+                      });
+                    },
+                  ),
+                  Text("Remember me"),
+                ],
+              ),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: _onTapSignInButton,
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: "Mulish",
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  void _onTapSignInButton(){
+
   }
 }
